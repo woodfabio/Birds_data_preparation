3fbee2e435f4625e9978b8f53e04631a3f27d267
fabiowood <fabio.wood.rs@gmail.com>
2023-08-01 11:54:45 -0300

    setwd_command_added

--- R_scripts/segmts_and_sites_capitalization.R
+++ R_scripts/segmts_and_sites_capitalization.R
@@ -1,17 +1,21 @@
   
-## Script para correcao/uniformizacao da capitalizacao de letras nos nomes dos
-## sitios nos arquivos .csv "date_time_diurnal.txt" e "locations_diurnal.txt",
-## que se tornarao, respectivamente, os objetos"SEGMTS" e "SITES" no script
-## "Formatadados_r.R"
+## Script to correct/standardize sites names letter capitalization in the CSV
+## files "date_time_diurnal.txt" and "locations_diurnal.txt" (in the folder
+## "Data_input/Raw_data), which will become, respectively, the objects "SEGMTS"
+## and "SITES" in the script "data_preparation_r.R".
 
 # ------------------------------------------------------------------------------
-## Carregar arquivos:
+## Load files:
 
-SEGMTS <- read.csv("../Data_input/date_time_diurnal.txt", head=TRUE, sep=",")
-SITES <- read.csv("../Data_input/locations_diurnal.txt", head=TRUE, sep="\t", check.names = FALSE)
+# First we need to set the working directory using the command "setwd()", ex.:
+
+# setwd("C:/Users/fabio/OneDrive/Documentos/Projetos_github/birds_data_preparation/R_scripts")
+
+SEGMTS <- read.csv("../Data_input/Raw_data/date_time_diurnal.txt", head=TRUE, sep=",")
+SITES <- read.csv("../Data_input/Raw_data/locations_diurnal.txt", head=TRUE, sep="\t", check.names = FALSE)
 
 # ------------------------------------------------------------------------------
-## Corrigir capitalizaçao:
+## Correct/standardize capitalization:
 
 SEGMTS$Spot <- sub("(?<=^.{2})C", "c", SEGMTS$Spot, perl = TRUE)
 SEGMTS$Spot <- sub("(?<=^.{2})M", "m", SEGMTS$Spot, perl = TRUE)
@@ -22,7 +26,7 @@ SITES$Spot <- sub("(?<=^.{2})M", "m", SITES$Spot, perl = TRUE)
 SITES$Spot <- sub("(?<=^.{7})X", "x", SITES$Spot, perl = TRUE)
 
 # ------------------------------------------------------------------------------
-## Exportar arquivos corrigidos:
+## Export corrected files:
 
 write.table(SEGMTS, "../Data_input/date_time_diurnal_capitalization_r.txt", sep = ",", row.names=FALSE, quote=FALSE)
 write.table(SITES, "../Data_input/locations_diurnal_capitalization_r.txt", sep = "\t", row.names=FALSE, quote=FALSE)
